cf0b94355a44bf350f21607d202c814c2e1937af
rachelkelly <rkellyalso@gmail.com>
2016-06-05 17:40:42 -0700

    final hamming work

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -4,9 +4,6 @@ def distance(strand1, strand2):
     hamcount = 0
 
     for i, item in enumerate(strand1):
-        if strand1[i] == strand2[i]:
-            pass
-        else:
-            strand1[i] != strand2[i]
+        if strand1[i] != strand2[i]:
             hamcount += 1
     return hamcount
