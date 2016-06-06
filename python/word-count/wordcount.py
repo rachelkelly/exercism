9d85f2b0e824f7b733824da9d75ae403c5d4d0ae
rachelkelly <rkellyalso@gmail.com>
2016-06-05 18:42:34 -0700

    adding to a dictionary when you want to\ change a value but not a key is hard

--- python/word-count/wordcount.py
+++ python/word-count/wordcount.py
@@ -4,10 +4,10 @@
 # Word Count exercism - Python
 #
 
-# handle utf-8
 # Create list of incoming sentence with a for loop list append
-# Create empty dict.  For each new word add to dict with a 1.
+# Create empty dict.  For each new word add key to dict with value 1.
  # if word already in dict, integer++ for dict item value
+# for each word, return a count
 
 def word_count(sentence):
     
@@ -16,17 +16,25 @@ def word_count(sentence):
     for i in sentence:
         sentlist.append(i)
 
-    # populate new dict
+    # populate or add to new dict
     sentdict = {}
     for key in sentlist: # iterate over every key in dict
         if key in sentdict: # if we've seen this key before, iterate up its value
-            for i in get(key[, i]) # not sure about this syntax
-                j = i + 1              # make a quick new var to plug in
-                sentdict.update(key=j) # 
+            #for key, j in sentdict.iteritems(key, j):
+            #    sentdict.update(key=(j+1))
+            #for i in key[i]: # not sure about this syntax
+            #    j = i + 1              # make a quick new var to plug in
+            #    sentdict.update(key=j) # or sentdict[key] = j ?
             #sentdict.update(key=(value+=1))
             # increment value by 1
             pass
         else: # else condition only happens if it's the first time the word's been seen
             sentdict[key] = 1 # should only happen once
+            sentdict.viewitems()
+        # if/else conditional happens for each word
+
+    # return count
+    for key in sentdict:
+        pass        
 
 word_count('The kids are okay kids.')
