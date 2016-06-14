59a62d112b20bbb3547600f0a59384632533b58b
rachelkelly <rkellyalso@gmail.com>
2016-06-13 18:39:52 -0700

    still working on the one thing

--- python/word-count/wordcount.py
+++ python/word-count/wordcount.py
@@ -4,19 +4,10 @@
 # Word Count exercism - Python
 #
 
-# ! DONE # Create list of incoming sentence with a for loop list append
-# ! DONE # Create empty dict.  For each new word add key to dict with value 1.
- # if word already in dict, integer++ for dict item value
-# for each word, return a count
-# ! DONE # account for case too!
-# ! DONE # gotta put WORDS into the list, not letters.
-
-# need to import the string lib?
 from string import split
 
 def word_count(sentence):
     
-    # turn the sent into a list for parsing
     sentlist = split(sentence.lower()) # split sentence into distinct words & lowercase them all
 
     # populate or add to new dict - create the empty dict, analyze each element 
@@ -24,12 +15,16 @@ def word_count(sentence):
     sentdict = {}
     for key in sentlist: # key is just iterator to be consistent throughout conditional
         if key in sentdict: # if we've seen this key before, increment its value
-            sentdict[key] = 2 # need to generically refer to its value in order to increment value
+            sentdict[key] = 2 # this will only work once
+            #for key, value in sentdict.iteritems():
+            #    value += 1 # this is NOT currently incrementing the value
+            print "repeat"
         else:
             sentdict[key] = 1
+        print sentdict
 
     # return count
     return sentdict
 
 # just for my own testing
-word_count('The kids are okay kids.')
+word_count('The the the kids are okay kids')
