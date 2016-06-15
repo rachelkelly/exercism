933574430df52598bd688f558350b8df997b1787
rachelkelly <rkellyalso@gmail.com>
2016-06-14 17:27:09 -0700

    might have a better tack but still probably need help

--- python/word-count/wordcount.py
+++ python/word-count/wordcount.py
@@ -13,13 +13,12 @@ def word_count(sentence):
     # populate or add to new dict - create the empty dict, analyze each element 
     # in sentlist, ++ value if seen more than once
     sentdict = {}
-    for key in sentlist: # key is just iterator to be consistent throughout conditional
-        if key in sentdict: # if we've seen this key before, increment its value
-            sentdict[key] = 2 # this will only work once
-            #for key, value in sentdict.iteritems():
-            #    value += 1 # this is NOT currently incrementing the value
+    for key in sentlist: 
+        if key in sentdict:
+            value = sentdict[key]
+            value += 1
             print "repeat"
-        else:
+        elif key not in sentdict:
             sentdict[key] = 1
         print sentdict
 
