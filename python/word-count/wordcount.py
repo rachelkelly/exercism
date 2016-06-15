428fd4cb638d59731efc4bad26d31aec02619068
rachelkelly <rkellyalso@gmail.com>
2016-06-15 10:29:52 -0700

    solved problem of word count & dictionary value change!!!

--- python/word-count/wordcount.py
+++ python/word-count/wordcount.py
@@ -7,23 +7,34 @@
 from string import split
 
 def word_count(sentence):
-    
-    sentlist = split(sentence.lower()) # split sentence into distinct words & lowercase them all
+
+    for i in sentence:
+        if i in ("\"\\'!@#$%^&*():;,.[]|{}-_=+`~?"):
+            sentence.replace(sentence[i], ' ')
+
+# oof below
+#    sentenceprocessing = list(sentence)
+#    for i in sentenceprocessing:
+#        if i in ("\"\\'!@#$%^&*():;,.[]|{}-_=+`~?"):
+#            sentenceprocessing[i] = " "
+#    sentence = "".join(sentenceprocessing)    
+#    for i in sentence:
+#        if i in ("\"\\'!@#$%^&*():;,.[]|{}-_=+`~?"):
+#            print i
+#            i = " "
+    sentlist = split(sentence.lower())
 
     # populate or add to new dict - create the empty dict, analyze each element 
     # in sentlist, ++ value if seen more than once
     sentdict = {}
     for key in sentlist: 
         if key in sentdict:
-            value = sentdict[key]
-            value += 1
-            print "repeat"
+            sentdict[key] += 1
         elif key not in sentdict:
             sentdict[key] = 1
-        print sentdict
 
     # return count
     return sentdict
 
 # just for my own testing
-word_count('The the the kids are okay kids')
+#word_count('The the the kids are okay kids.')
