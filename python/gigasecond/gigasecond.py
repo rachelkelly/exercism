933c3c9569791cfa90986852694222cb9c233898
Rachel Kelly <rkellyalso@gmail.com>
2016-06-27 17:02:03 -0700

    cleaned it up mightily, perhaps TOO much

--- python/gigasecond/gigasecond.py
+++ python/gigasecond/gigasecond.py
@@ -4,9 +4,5 @@
 
 import datetime
 
-# time_of_birth is a 5-parameter variable
 def add_gigasecond(time_of_birth):
-    print 'Date of Birth including time:', time_of_birth
-    gs = datetime.timedelta(seconds=1000000000)
-    gs_day = time_of_birth + gs
-    return gs_day #always return from a func
+    return time_of_birth + datetime.timedelta(seconds=10**9)
