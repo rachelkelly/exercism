82dfccf49aaebb8be16ca8b5cadb9e90070049ad
Rachel Kelly <rkellyalso@gmail.com>
2016-06-28 14:43:21 -0700

    All but 3 tests out of 20 passing

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,13 +1,18 @@
-#
-# Skeleton file for the Python "Bob" exercise.
-#
+# Python "Bob" exercism exercise
 
+import string
 
 def hey(what):
-    if '?' in what:
+    if what.isupper():
+        return 'Whoa, chill out!'
+    elif what.endswith('?'):
         return 'Sure.'
+    elif "Let's" in what:
+        return 'Whatever.'
     elif '!' in what:
         return 'Whoa, chill out!'
+    elif what == '':
+        return 'Fine. Be that way!'
     else:
         return 'Whatever.'
 
