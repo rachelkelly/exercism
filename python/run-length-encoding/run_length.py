5b3df24785d880d88e9c122ea2a348088d47dfa2
Rachel Kelly <rkellyalso@gmail.com>
2016-06-30 12:07:24 -0700

    skeletal file worked out - two funcs, thinking list creation/iteration & then string concatenation.

---
+++ python/run-length-encoding/run_length.py
@@ -0,0 +1,11 @@
+# Python "Run Length Encoding" exercism exercise
+# "AABCCCDEEEE"  ->  "2AB3CD4E"  ->  "AABCCCDEEEE"
+
+def encode(plain):
+    pass
+    # for i in plain, add to list, count list, concatenate into string
+
+def decode(enc):
+    pass
+
+encode('AAB')
